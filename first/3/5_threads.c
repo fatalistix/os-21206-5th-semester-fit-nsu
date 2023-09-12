56c8d8bee729b14eb62a8448a54906244920c70b
fatalistix <slavabal11@outlook.com>
2023-09-12 14:44:17 +0700

    like thread.c, but with more threads

---
+++ first/3/5_threads.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023, Balashov Vyacheslav
+ */
+#define _GNU_SOURCE
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *mythread(void *arg) {
+  printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(),
+         gettid());
+  return NULL;
+}
+
+#define NUM_THREADS 5
+
+int main() {
+  pthread_t tids[NUM_THREADS];
+  int err;
+
+  printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+  for (int i = 0; i < NUM_THREADS; ++i) {
+    err = pthread_create(&(tids[i]), NULL, mythread, NULL);
+    if (err) {
+      printf("main: pthread_create() failed: %s\n", strerror(err));
+      return -1;
+    }
+  }
+
+  for (int i = 0; i < NUM_THREADS; ++i) {
+    pthread_join(tids[i], NULL);
+  }
+
+  return 0;
+}
