01a5678565359800a4b5e2f9ba9f6da583fe8da1
fatalistix <slavabal11@outlook.com>
2023-09-19 12:56:25 +0700

    completed second task

---
+++ first/3-grade/1_2/g.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023, Balashov Vyacheslav
+ */
+
+#include <asm-generic/errno-base.h>
+#define _GNU_SOURCE
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *thread_foo(void *arg) {
+  // sleep(10);
+  printf("thread: finished\n");
+  return NULL;
+}
+
+int main() {
+  pthread_t tid;
+  int err;
+  pthread_attr_t attr;
+
+  err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+  if (err) {
+    perror("main: setting attr");
+    return -1;
+  }
+
+  err = pthread_create(&tid, &attr, thread_foo, NULL);
+  if (err) {
+    perror("main: error creating thread\n");
+    return -1;
+  }
+
+  // err = pthread_detach(tid);
+  // if (err) {
+  // perror("main: error detaching thread\n");
+  // return -1;
+  // }
+
+  sleep(10);
+
+  err = pthread_join(tid, NULL);
+  if (err == EINVAL) {
+    printf("THREAD IS DETACHED\n");
+  }
+  return 0;
+}
